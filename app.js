eeaea7b5e68fd02eabcd1e0142259a969846d3c4
asimov <seyranmustafa4@gmail.com>
2025-01-09 13:48:48 +0300

    p

--- app.js
+++ app.js
@@ -21,8 +21,29 @@ app.use(express.json())
 // express-async-errors: catch async-errors and send to errorHandler:
 require('express-async-errors')
 
+
+
 /* ------------------------------------------------------- */
+
+
+
+app.set("view engine","ejs")
+/* ------------------------------------------------------- */
+
+
+
+
+
+
+
 //* Routes:
+
+app.all("/",(req,res)=>{
+    res.send({
+        error:false,
+        message:"Welcome to TodoAPI",
+    })
+})
 
 app.use('/todos', require('./app/routes/todo.router'))
 

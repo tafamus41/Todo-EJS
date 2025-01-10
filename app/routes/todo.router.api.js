16e314b1068793a928d2c1bffdbded3819b9c4b9
asimov <seyranmustafa4@gmail.com>
2025-01-10 07:16:28 +0300

    p

--- app/routes/todo.router.api.js
+++ app/routes/todo.router.api.js
@@ -10,17 +10,8 @@ const router = require('express').Router()
 // Call todo.controller:
 const todo = require('../controllers/todo.controller.api')
 
-// // LIST TODOS:
-// router.get('/', todo.list)
+
 // //? CRUD -> 
-// // CREATE TODO:
-// router.post('/', todo.create)
-// // READ TODO:
-// router.get('/:id(\\d+)', todo.read)
-// // UPDATE TODO:
-// router.put('/:id', todo.update)
-// // DELETE TODO:
-// router.delete('/:id', todo.delete)
 
 router.route('/')
     .get(todo.list)
